c64800283e31167c31d6a8de16fd37e0ac004c17
Justin Lin <wancat@wancat.cc>
2026-09-20 13:56:37 +1000

    feat: add holding bow

--- scripts/arrow.gd
+++ scripts/arrow.gd
@@ -3,11 +3,13 @@ extends Area2D
 class_name Arrow
 
 @export var direction: Vector2
+@onready var timer: Timer = $Timer
 
 const SPEED = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	timer.start()
 	pass # Replace with function body.
 
 
@@ -17,7 +19,11 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	destroy()
-	
 
 func destroy() -> void:
 	queue_free()
+
+
+func _on_timer_timeout() -> void:
+	print("timeout")
+	destroy()
--- scripts/bow.gd
+++ scripts/bow.gd
@@ -4,18 +4,44 @@ class_name Bow
 
 var direction = 1
 const angular_speed = 5
+const hold_speed = 2
+var hold: bool = false
 
 signal shoot_arrow(muzzle_pos: Vector2, direction: float)
 
 @onready var muzzle: Node2D = $Muzzle
+@onready var sprite: AnimatedSprite2D = $Sprite2D
+@onready var timer: Timer = $Timer
+@onready var arrow_sprite: Sprite2D = $ArrowSprite
+
+
+func _ready() -> void:
+	arrow_sprite.hide()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rotation += direction * angular_speed * delta
-
-
+	if hold:
+		rotation += direction * hold_speed * delta
+	else:
+		rotation += direction * angular_speed * delta
+	
 
 func _input(event: InputEvent) -> void:  
 	if event.is_action_pressed("fire"):
-		direction *= -1		
+		hold = true
+		sprite.play("hold")
+		direction *= -1
+		timer.start()
+		arrow_sprite.show()
+		
+	elif event.is_action_released("fire"):
+		hold = false
+		sprite.play("idle")
+		arrow_sprite.hide()
 		shoot_arrow.emit(muzzle.global_position, rotation)
+
+
+func _on_timer_timeout() -> void:
+	if hold:
+		direction *= -1
